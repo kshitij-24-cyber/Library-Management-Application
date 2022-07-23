d277504db3ab4beef3b1deedbeeef2759163cedc
kshitij-24-cyber <68536909+kshitij-24-cyber@users.noreply.github.com>
2022-07-23 17:03:41 +0530

    created GetMapping

---
+++ src/main/java/com/example/librarymanagementapplication/exception/BookNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.librarymanagementapplication.exception;/*
+ * Author Name:Kshitij sahu
+ * IDE: intellij IDEA Community Edition
+ * Date: 23-07-2022
+ */
+
+public class BookNotFoundException extends Throwable {
+}
